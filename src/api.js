211c22dbec54db7c5a65f72d5806fe7aee6a0d06
vlad-dragos-iustin-30125 <100029234+vlad-dragos-iustin-30125@users.noreply.github.com>
2023-04-12 01:36:51 +0300

    Done

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "329fa700a9msh5e81b3ac9783d80p12b283jsn4fe504af54e2",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "34326962cb63df165addaedd87fc8ddb";
